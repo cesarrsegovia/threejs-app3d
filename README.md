e942767c423995a6818c27b8c3d9169cfaa6937b
Cesar Segovia <cesarr.segovia27@gmail.com>
2023-07-14 18:01:48 -0300

    threejs app

---
+++ README.md
@@ -0,0 +1,6 @@
+#MERN-AI 3D app
+
+-Vite + React
+-Node - Express
+-Tailwind
+-ThreeJS 3D
---
+++ client/src/store/index.js
@@ -0,0 +1,13 @@
+import { proxy } from "valtio";
+
+const state = proxy({
+    intro: true,
+    color: '#EFBD48',
+    isLogoTexture: true,
+    isFullTexture: false,
+    logoDecal: './threejs2.png',
+    fullDecal: './threejs2.png'
+
+})
+
+export default state;
